402eb36ecf36f88d67a579c2e6452c2a922ac060
Sourav Sanjay <souravsanjay07@gmail.com>
2026-02-02 14:55:07 +0530

    backend api routes completed

--- Day8_Notes-App-React-DB/Backend/src/server.ts
+++ Day8_Notes-App-React-DB/Backend/src/server.ts
@@ -3,23 +3,50 @@ import "dotenv/config"
 import connectToDb from "./config/db";
 import { Request, Response } from "express";
 import { noteModel } from "./models/noteModel";
-
+import express from "express"
 
 connectToDb();
+app.use(express.json());
 
 interface notesBodyInterface{
     title: string
     description: string 
 }
 
-app.post("/notes", (req: Request, res: Response)=>{
-    let {title, description}: notesBodyInterface = req.body;
-    let note = noteModel.create({
+app.post("/api/notes", async (req: Request, res: Response)=>{
+    const {title, description}: notesBodyInterface = req.body;
+    let note = await noteModel.create({
         title,
         description
     })
     res.status(201).json({
         message: "Note created successfully",
+    })
+})
+
+app.get("/api/notes", async (req: Request, res: Response)=>{
+    let notes = await noteModel.find();
+    res.status(200).json({
+        notes
+    })
+})
+
+app.patch("/api/notes/:id", async (req: Request, res: Response)=>{
+    const {id} = req.params
+    const {title, description} : notesBodyInterface = req.body;
+
+    await noteModel.findByIdAndUpdate(id, {title, description});
+
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+})
+
+app.delete("/api/notes/:id", async(req: Request, res: Response)=>{
+    const {id} = req.params;
+    await noteModel.findByIdAndDelete(id);
+    res.status(200).json({
+        message: "Note deleted successfully"
     })
 })
 
