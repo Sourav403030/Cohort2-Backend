00b436c7bbb868ff4be8e245982fd4639fceb6c7
Sourav Sanjay <souravsanjay07@gmail.com>
2026-02-09 14:21:28 +0530

    Login route completed

--- Day10_Auth-Register-Login-Logout/src/app.ts
+++ Day10_Auth-Register-Login-Logout/src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import connectToDB from "./config/db";
 import { authRouter } from "./routes/authRoutes";
+import cookieParser from "cookie-parser"
 
 connectToDB();
 
 export const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
--- Day10_Auth-Register-Login-Logout/src/routes/authRoutes.ts
+++ Day10_Auth-Register-Login-Logout/src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express"
 import { User, userModel } from "../models/userModel";
 import bcrypt from "bcrypt"
 import generateOtp from "../helpers/otp";
+import jwt from "jsonwebtoken"
 
 export const authRouter =  express.Router();
 
@@ -10,7 +11,7 @@ authRouter.post("/register", async (req: Request, res: Response)=>{
     const result = User.safeParse(req.body);
 
     if(!result.success){
-        res.status(401).json({
+        return res.status(401).json({
             message: "Not valid credentials",
             error: result.error
         })
@@ -18,7 +19,7 @@ authRouter.post("/register", async (req: Request, res: Response)=>{
 
     const existingUser = await userModel.findOne({email});
     if(existingUser){
-        res.status(409).json({
+        return res.status(409).json({
             message: "User already exists",
         })
     }
@@ -37,4 +38,50 @@ authRouter.post("/register", async (req: Request, res: Response)=>{
         message: "User created successfully",
         newUser
     })
+})
+
+authRouter.post("/login", async (req: Request, res: Response)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({
+            message: "Email and Password required",
+        })
+    }
+
+    const user = await userModel.findOne({email});
+    if(!user){
+        return res.status(401).json({
+            message: "No user exists",
+        })
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if(!comparePassword){
+        return res.status(401).json({
+            message: "Incorrect Email or Password",
+        })
+    }
+
+    const JWT_SECRET = process.env.JWT_SECRET;
+
+    if(!JWT_SECRET){
+        return res.status(400).json({
+            message: "No JWT Secret provided"
+        })
+    }
+
+    const token = jwt.sign({id: user._id}, JWT_SECRET);
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+        message: "User LoggedIn Successfully"
+    })
+
 })
